6c20091a33c7e096521c3287615441031ca01407
Hoegsgaard <mathiashoegsgaard@gmail.com>
2018-06-07 16:23:37 +0200

    p5

--- Client/js/sketch.js
+++ Client/js/sketch.js
@@ -1,9 +1,10 @@
 function setup() {
   // put setup code here
-    createCanvas(640,400);
+    createCanvas(600,600);
 }
 
 function draw() {
   // put drawing code here
+    background(233,0,0);
     rect(100,100,200,200);
 }
