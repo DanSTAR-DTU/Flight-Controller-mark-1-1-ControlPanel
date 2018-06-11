830129d95cb7f65d205b0d0b43188d8065676907
Hoegsgaard <mathiashoegsgaard@gmail.com>
2018-06-11 22:29:15 +0200

    skala rettelser og en gaffel mere

--- Public/sketch.js
+++ Public/sketch.js
@@ -36,11 +36,11 @@ function setup(){
     fill(textColor);
     text("Oxidizer Side",(windowWidth/3)+10,(windowHeight/4)+15);
 
-    //Engine
+    //Engine or Specifications
     fill(50,50,125);
     rect((windowWidth/3)*2,0,windowWidth,windowHeight);
     fill(textColor);
-    text("Engine Side",(windowWidth/3*2)+10,15);
+    text("Engine or Specifications",(windowWidth/3*2)+10,15);
 
     //Abort
     fill(150,0,0);
@@ -78,39 +78,40 @@ function draw(){
     strokeWeight(5);
     fill(200,36,183);
     stroke(0);
-    line(windowWidth/3,40,windowWidth/3,windowHeight/4-60);
-    line((windowWidth/3)/2,windowHeight/4-60,(windowWidth/2),windowHeight/4-60);
-    line(windowWidth/3,windowHeight/4-75,windowWidth/6*0.25,windowHeight/4-75);
+    line(windowWidth/3,(windowHeight/4)*0.25,windowWidth/3,(windowHeight/4)*0.95);// main down
+    line((windowWidth/3)/2,(windowHeight/4)*0.95,(windowWidth/2),(windowHeight/4)*0.95);// vandrat forbinder Oxidizer og fuel
+    line(windowWidth/3,windowHeight/30+135,windowWidth/6*0.25,windowHeight/30+135);// vandrat go under fuel
     //fill
-    line(windowWidth/3+120,60,windowWidth/3+120,111)
-    line(windowWidth/3-80,115,windowWidth/3+115,115);
+    line(windowWidth/3+120,windowHeight/4*0.17,windowWidth/3+120,(windowHeight/30+71)-2)// lodrat
+    line(windowWidth/3-80,windowHeight/30+71,windowWidth/3+115,windowHeight/30+71);// vandrat
     fill(255);stroke(255);
-    triangle(windowWidth/3+115,105,windowWidth/3+125,105,windowWidth/3+120,115);//fill
+    triangle(windowWidth/3+115,(windowHeight/30+71)-5,windowWidth/3+125,(windowHeight/30+71)-5,windowWidth/3+120,windowHeight/30+71);//fill
     stroke(0);fill(0);
-    triangle(windowWidth/3-90,115,windowWidth/3-80,110,windowWidth/3-80,120);
+    triangle(windowWidth/3-90,windowHeight/30+71,windowWidth/3-80,(windowHeight/30+71)-5,windowWidth/3-80,(windowHeight/30+71)+5);//vandrat
     fill(200,36,183);
     noStroke();
     //Tap
     stroke(0);
-    line(windowWidth/3,130,windowWidth/3-198,130);
+    line(windowWidth/3,windowHeight/30+96,windowWidth/3-198,windowHeight/30+96);
     fill(255);stroke(255);
-    triangle(windowWidth/3-200,130,windowWidth/3-190,135,windowWidth/3-190,125);//Vinkelret vandrat
-    triangle(windowWidth/3-204,126,windowWidth/3-199,116,windowWidth/3-209,116);//Vinkelret lodrat
+    triangle(windowWidth/3-200,windowHeight/30+96,windowWidth/3-190,(windowHeight/30+96)-5,windowWidth/3-190,(windowHeight/30+96)+5);//Vinkelret vandrat
+    triangle(windowWidth/3-204,(windowHeight/30+96)-5,windowWidth/3-199,(windowHeight/30+96)-15,windowWidth/3-209,(windowHeight/30+96)-15);//Vinkelret lodrat
+    //tank
     fill(200,36,183);
     noStroke();
     beginShape()
-    arc(windowWidth/3,30,100,30,PI,0,PIE);
-    rect(windowWidth/3-50,30,100,50);
-    arc(windowWidth/3,80,100,30,0,PI,PIE);
+    arc(windowWidth/3,windowHeight/30,100,30,PI,0,PIE);
+    rect(windowWidth/3-50,windowHeight/30,100,50);
+    arc(windowWidth/3,windowHeight/30+49,100,30,0,PI,PIE);
     endShape();
 
 
     //Fuel Side
     stroke(0);
-    line((windowWidth/3)/2,windowHeight/4-60,(windowWidth/3)/2,windowHeight/2);
+    line((windowWidth/3)/2,(windowHeight/4)*0.95,(windowWidth/3)/2,windowHeight/2);// DPR to fuel tank
     line((windowWidth/3)/2,windowHeight/2,(windowWidth/3)/2,windowHeight-60);
-    line(windowWidth/6*0.25,windowHeight/4-75,windowWidth/6*0.25,windowHeight-80);
-    line(windowWidth/6*0.25,windowHeight-79,(windowWidth/3)/2,windowHeight-80);
+    line(windowWidth/6*0.25,windowHeight/30+135,windowWidth/6*0.25,windowHeight-80);
+    line(windowWidth/6*0.25,windowHeight-79,(windowWidth/3)/2,windowHeight-80);// vandret under tank fra DRP
     line(windowWidth/6*0.25,windowHeight-110,windowWidth/2,windowHeight-110)//nederste tværgående
     //Fill
     line(windowWidth/6*0.6,windowHeight/2*0.75,windowWidth/6*0.6,windowHeight/2+190)
@@ -131,7 +132,6 @@ function draw(){
     line(windowWidth/6*1.2,windowHeight/8*3.2,windowWidth/6*1.55,windowHeight/8*3.2)//ned vandrat
     noStroke()
 
-
     //Tank
     beginShape();
     arc((windowWidth/3)/2,windowHeight/2,100,30,PI,0,PIE);
@@ -142,7 +142,7 @@ function draw(){
 
     //Oxidizer Side
     stroke(0);
-    line(windowWidth/2-0.1,windowHeight/4-60,windowWidth/2-0.1,windowHeight/2);
+    line(windowWidth/2-0.1,(windowHeight/4)*0.95,windowWidth/2-0.1,windowHeight/2);//DPR to oxidizer tank
     line(windowWidth/2+0.1,windowHeight/2,windowWidth/2+0.1,windowHeight-60);
     //Fill
     line(windowWidth/6*2.6,windowHeight/2*0.75,windowWidth/6*2.6,windowHeight/2+190)
@@ -150,8 +150,25 @@ function draw(){
     fill(255);stroke(255);
     triangle(windowWidth/6*2.6-5,windowHeight/2*0.8,windowWidth/6*2.6+5,windowHeight/2*0.8,windowWidth/6*2.6,windowHeight/2*0.8+10)//fill
     triangle(windowWidth/2-4,windowHeight-60,windowWidth/2+6,windowHeight-60,windowWidth/2+1,windowHeight-50);
-    fill(200,36,183);stroke(0);
-    noStroke();
+    fill(200,36,183);
+    //Fork
+        stroke(0);
+        //op
+        line(windowWidth/6*3.3,windowHeight/8*3,windowWidth/6*3.3,windowHeight/8*2.5)//op lodrat 1
+        line(windowWidth/6*3.3,windowHeight/8*2.5,windowWidth/6*3.5,windowHeight/8*2.5)//op vandrat
+        line(windowWidth/6*3.5,windowHeight/8*2.5,windowWidth/6*3.5,windowHeight/8*2.3)//op lodrat 2
+        //mellem
+        line(windowWidth/2-0.1,windowHeight/8*3,windowWidth/6*3.7,windowHeight/8*3);//mellem
+        line(windowWidth/6*3.7,windowHeight/8*3,windowWidth/6*3.7,windowHeight/8*2.7);//mellem ekstra lodrat 1
+        line(windowWidth/6*3.7,windowHeight/8*2.7,windowWidth/6*3.85,windowHeight/8*2.7)//mellem ekstra vandrat
+        line(windowWidth/6*3.85,windowHeight/8*2.7,windowWidth/6*3.85,windowHeight/8*2.3)
+        //ned
+        line(windowWidth/6*3.2,windowHeight/8*3,windowWidth/6*3.2,windowHeight/8*3.2); //ned lodrat
+        line(windowWidth/6*3.2,windowHeight/8*3.2,windowWidth/6*3.55,windowHeight/8*3.2)//ned vandrat
+        noStroke()
+        //trekaner
+    
+    //tank
     beginShape();
     arc((windowWidth/2),windowHeight/2,100,30,PI,0,PIE);
     rect((windowWidth/2)-50,windowHeight/2,100,150)
