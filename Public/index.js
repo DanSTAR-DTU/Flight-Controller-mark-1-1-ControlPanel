ec8da837814c6915543e8124259325a931688f82
Tobias Jessen <s174292@student.dtu.dk>
2018-07-27 00:00:43 +0200

    Added status texts to index.html

---
+++ Public/index.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+var socketIOLabel = document.getElementById("con_label_socketio");
+var beagleLabel = document.getElementById("con_label_beagle");
+
+setLabelStatus(socketIOLabel, false);
+setLabelStatus(beagleLabel, false);
+
+socket.on("connect", () => {
+    setLabelStatus(socketIOLabel, true);
+});
+
+socket.on("disconnect", () => {
+    setLabelStatus(socketIOLabel, false);
+});
+
+function setLabelStatus(label, connected) {
+    if (connected) {
+        label.innerHTML = "True";
+        label.classList.remove("notconnected");
+        label.classList.add("connected");
+    } else {
+        label.innerHTML = "False";
+        label.classList.remove("connected");
+        label.classList.add("notconnected");
+    }
+}
