97596286e8d89dd4aa773a0c788a7d2d2af8071f
futte111 <ntthyssen@gmail.com>
2018-08-06 09:59:33 +0200

    Merge branch 'master' of /home/futte/WebstormProjects/Flight-Controller-mark-1-1-ControlPanel with conflicts.

--- Public/rocket.js
+++ Public/rocket.js
@@ -25,50 +25,42 @@ socket.on('info', function (data) {
 });
 //'/([^,]+)([^;]+)/g'
 */
-var IO_IP = "0.0.0.0";
-var IO_PORT = 3000;
-var socket = io(IO_IP + ":" + IO_PORT);
 
-socket.on("info", function(data) {
-    console.log(data);
-});
-
-//control of vent 1
 var a = document.getElementById("DataSVG");
 var stateArray = []
-
+var valvesArr = []
+//sets alle valves to closed state
 function preLoad() {
     var svgDoc = a.contentDocument;
     for (var i = 1; i <= 6; i++) {
-        svgDoc.getElementById('vent'+i+'_text').textContent = "OFF";
+        svgDoc.getElementById('vent'+i+'_text').textContent = "CLOSED";
         svgDoc.getElementById('vent'+i+'_text').style.fill = 'red';
+
         stateArray[i] = false
 
     }
 
 }
-
-
+// listensten to all click events on vents
 a.addEventListener('load', function () {
-    preLoad()
-    vents(1)
-    vents(2)
-    vents(3)
-    vents(4)
-    vents(5)
-    vents(6)
+    preLoad();
+    valves(1);
+    valves(2);
+    valves(3);
+    valves(4);
+    actuator();
 });
 
-function vents(i) {
+function valves(i) {
     var svgDoc = a.contentDocument
-    var delta = svgDoc.getElementById("vent"+i);
+    var delta = svgDoc.getElementById("vent2-"+i);
     delta.addEventListener("click", function () {
         if (stateArray[i] == false) {
-            svgDoc.getElementById('vent'+i+'_text').textContent = 'ON';
+            svgDoc.getElementById('vent'+i+'_text').textContent = 'OPEN';
             svgDoc.getElementById('vent'+i+'_text').style.fill = 'green';
             stateArray[i] = true
         } else {
-            svgDoc.getElementById('vent'+i+'_text').textContent = "OFF";
+            svgDoc.getElementById('vent'+i+'_text').textContent = "CLOSED";
             svgDoc.getElementById('vent'+i+'_text').style.fill = 'red';
             stateArray[i] = false
         }
@@ -77,3 +69,36 @@ function vents(i) {
 }
 
 
+function actuator() {
+    var svgDoc = a.contentDocument;
+    var delta = document.getElementById("openValveButton1")
+    delta.addEventListener("click", function () {
+        var input = document.getElementById('openValve1')
+        console.log(input.value)
+        if(input.value == 'close'){
+            svgDoc.getElementById('vent5_text').textContent = "CLOSED";
+            svgDoc.getElementById('vent5_text').style.fill = 'red';
+        }else if (input.value == 'open') {
+            svgDoc.getElementById('vent5_text').textContent = 'OPEN';
+            svgDoc.getElementById('vent5_text').style.fill = 'green';
+        }else{
+            svgDoc.getElementById('vent5_text').textContent = input.value;
+            svgDoc.getElementById('vent5_text').style.fill = 'green';
+        }
+    })
+    var gamma = document.getElementById("openValveButton2")
+    gamma.addEventListener("click", function () {
+        var input = document.getElementById('openValve2')
+        console.log(input.value)
+        if(input.value == 'close'){
+            svgDoc.getElementById('vent6_text').textContent = "CLOSED";
+            svgDoc.getElementById('vent6_text').style.fill = 'red';
+        }else if (input.value == 'open') {
+            svgDoc.getElementById('vent6_text').textContent = 'OPEN';
+            svgDoc.getElementById('vent6_text').style.fill = 'green';
+        }else{
+            svgDoc.getElementById('vent6_text').textContent = input.value;
+            svgDoc.getElementById('vent6_text').style.fill = 'green';
+        }
+    })
+}
